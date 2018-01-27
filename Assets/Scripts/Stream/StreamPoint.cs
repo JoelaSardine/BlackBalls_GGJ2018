cf4e2b9caa5b1d827e44939d3386c726301af4d8
Grégoire Chamberland <gregoire.chamberland@gmail.com>
2018-01-27 19:25:31 +0100

    Spline stream points generator.

---
+++ Assets/Scripts/Stream/StreamPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreamPoint : MonoBehaviour {
+
+	public Vector3 Direction;
+
+	public void OnDrawGizmos() {
+		Gizmos.color = Color.blue;
+		Vector3 halfDirection = Direction / 2f;
+		Vector3 perpendicularQuarterDirection = new Vector3(-halfDirection.y, halfDirection.x) / 2f;
+		Gizmos.DrawLine (transform.position - halfDirection, transform.position + halfDirection);
+		Gizmos.DrawLine (transform.position + halfDirection, transform.position + perpendicularQuarterDirection);
+		Gizmos.DrawLine (transform.position + halfDirection, transform.position - perpendicularQuarterDirection);
+	}
+}
