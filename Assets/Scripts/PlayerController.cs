03740c521aa2a89586fefe73856eb5b84e79ce8f
Joe la Sardine <thibaut.poittevin@gmail.com>
2018-01-27 08:39:57 +0100

    Created PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackBalls
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private Rigidbody rb;
+
+        public float speedForce = 100f;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            // Receive Inputs
+
+            rb.AddForce(Input.GetAxis("Horizontal") * speedForce, Input.GetAxis("Vertical") * speedForce, 0);
+        }
+
+        void Move()
+        {
+
+        }
+    }
+}
